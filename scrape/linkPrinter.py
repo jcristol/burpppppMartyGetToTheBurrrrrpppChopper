842f7762e416214a04823644db496fdbae577dff
Joshua Cristol <jcrstiol@spredfast.com>
2017-07-28 17:22:48 -0500

    i like this pattern more

--- scrape/linkPrinter.py
+++ scrape/linkPrinter.py
@@ -13,10 +13,6 @@ response = urllib2.urlopen(base_url)
 html_doc = response.read()
 soup = BeautifulSoup(html_doc, 'html.parser')
 
-# '^/wiki/.*morty$'
-
 mortyTable = [e for e in soup.descendants if e.name == "table"][1]
-hrefATags = [e for e in mortyTable.descendants if e.name == "a" and "href" in e.attrs]
-mortyTags = [tag for tag in hrefATags if re.search('^/wiki/\w*$', tag['href'])]
-
+mortyTags = [tag for tag in mortyTable.descendants if tag.name == "a" and "href" in tag.attrs if re.search('^/wiki/\w+$', tag["href"])]
 pprint(mortyTags)
