690a7fd495028a3b7e6d42b368c9811f4bb52331
Joshua Cristol <jcrstiol@spredfast.com>
2017-07-28 17:36:37 -0500

    scrape is coming along all right

--- scrape/linkPrinter.py
+++ scrape/linkPrinter.py
@@ -1,15 +1,24 @@
 from bs4 import BeautifulSoup
+from pprint import pprint
 import urllib2
 import re
-import pprint
 
+baseUrl = 'http://rickandmorty.wikia.com'
+mortyListUrl = baseUrl + '/wiki/List_of_Mortys_(Pocket_Mortys)'
 
-base_url = 'http://rickandmorty.wikia.com/wiki/List_of_Mortys_(Pocket_Mortys)'
-
-response = urllib2.urlopen(base_url)
+response = urllib2.urlopen(mortyListUrl)
 html_doc = response.read()
 soup = BeautifulSoup(html_doc, 'html.parser')
 
 mortyTable = [e for e in soup.descendants if e.name == "table"][1]
 mortyTags = [tag for tag in mortyTable.descendants if tag.name == "a" and "href" in tag.attrs if re.search('^/wiki/\w+$', tag["href"])]
-pprint(mortyTags)
+
+for tag in mortyTags:
+    url = baseUrl + tag['href']
+    print "requesting " + url
+    response = urllib2.urlopen(url)
+    print "responded with " + str(response.getcode())
+    print
+
+# import code
+# code.interact(local=locals())
