eccd91ce8518a2dae5cd3dd75ea9dc65128a7090
Joshua Cristol <jcrstiol@spredfast.com>
2017-07-30 17:32:26 -0500

    made the soupify function

--- scrape/linkPrinter.py
+++ scrape/linkPrinter.py
@@ -4,21 +4,31 @@ import urllib2
 import re
 
 baseUrl = 'http://rickandmorty.wikia.com'
-mortyListUrl = baseUrl + '/wiki/List_of_Mortys_(Pocket_Mortys)'
+mortyTableUrl = baseUrl + '/wiki/List_of_Mortys_(Pocket_Mortys)'
 
-response = urllib2.urlopen(mortyListUrl)
-html_doc = response.read()
-soup = BeautifulSoup(html_doc, 'html.parser')
-
-mortyTable = [e for e in soup.descendants if e.name == "table"][1]
-mortyTags = [tag for tag in mortyTable.descendants if tag.name == "a" and "href" in tag.attrs if re.search('^/wiki/\w+$', tag["href"])]
-
-for tag in mortyTags:
-    url = baseUrl + tag['href']
-    print "requesting " + url
+def soupify(url):
     response = urllib2.urlopen(url)
-    print "responded with " + str(response.getcode())
-    print
+    return BeautifulSoup(response.read(), 'html.parser')
+
+soup = soupify(mortyTableUrl)
+mortyTable = [e for e in soup.descendants if e.name == "table"][1]
+mortyTableEntries = [tag for tag in mortyTable.descendants if tag.name == "a" and "href" in tag.attrs if re.search('^/wiki/\w+$', tag["href"])]
+
+testTag = mortyTableEntries[0]
+
+url = baseUrl + testTag['href']
+mortySoup = soupify(url)
+print mortySoup.prettify()
+
+
+
+
+# for tag in mortyTableEntries:
+#     url = baseUrl + tag['href']
+#     print "requesting " + url
+#     response = urllib2.urlopen(url)
+#     print "responded with " + str(response.getcode())
+#     print
 
 # import code
 # code.interact(local=locals())
