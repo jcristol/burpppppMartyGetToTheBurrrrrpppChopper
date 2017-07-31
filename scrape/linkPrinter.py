6a55f1c6a74c2c1d0ec14f44a3ddd0355cf1cc81
Joshua Cristol <jcrstiol@spredfast.com>
2017-07-30 19:17:43 -0500

    finally scraped the images with a poppy script i prolly want to make this thing somewhat better and faster to use test and debug

--- scrape/linkPrinter.py
+++ scrape/linkPrinter.py
@@ -1,48 +1,40 @@
 from bs4 import BeautifulSoup
 from pprint import pprint
-from urllib2 import urlopen, urlretrieve
-from re import search
+import re, urllib, urllib2
 
 baseUrl = 'http://rickandmorty.wikia.com'
 mortyTableUrl = baseUrl + '/wiki/List_of_Mortys_(Pocket_Mortys)'
 
 def soupify(url):
+    print "Request: " + url
     response = urllib2.urlopen(url)
+    print "Responded: " + str(response.getcode())
     return BeautifulSoup(response.read(), 'html.parser')
 
-def searchForTables(soup):
-    return [e for e in soup.descendants if e.name == "table"]
+def getPocketMortyImgUrlAndName(soup):
+    pocketMortyImageTableList = [e for e in soup.descendants if e.name == "table" and "class" in e.attrs and len(e["class"]) == 1 and e["class"][0] == "infobox-interior"]
+    if len(pocketMortyImageTableList) == 1:
+        imgTagList = [tag for tag in pocketMortyImageTableList[0].descendants if tag.name == "img" and "data-image-name" in tag.attrs and re.search('PM\-\d+\.png', tag["data-image-name"])]
+        if len(imgTagList) != 1:
+            return (None, None)
+        return (imgTagList[0]["src"], imgTagList[0]['data-image-name'])
+    return (None, None)
+
+def download(url, name):
+    if url is None or name is None:
+        return
+    print "Request Download from: " + url
+    urllib.urlretrieve(url, "images/" + name)
+    print "Downloaded: " + name
+    return
+
+
 
 #base wiki page with all the morty table entries
 soup = soupify(mortyTableUrl)
-mortyTable = searchForTables(soup)[1]
+mortyTable = [e for e in soup.descendants if e.name == "table"][1]
 mortyTableEntries = [tag for tag in mortyTable.descendants if tag.name == "a" and "href" in tag.attrs if re.search('^/wiki/\w+$', tag["href"])]
-
-testTag = mortyTableEntries[0]
-
-#inner morty image url
-testUrl = baseUrl + testTag['href']
-mortySoup = soupify(testUrl)
-pocketMortyImageTable = searchForTables(mortySoup)[0]
-imgTag = [tag for tag in pocketMortyImageTable.descendants if tag.name == "img" and "data-image-name" in tag.attrs and re.search('PM\-\d+\.png', tag["data-image-name"])][0]
-srcImageUrl = imgTag["src"]
-print srcImageUrl
-
-#download the image
-urllib2.urlretrieve(srcImageUrl, "test.png")
-
-
-
-
-
-# PM-002.png
-
-# for tag in mortyTableEntries:
-#     url = baseUrl + tag['href']
-#     print "requesting " + url
-#     response = urllib2.urlopen(url)
-#     print "responded with " + str(response.getcode())
-#     print
-
-# import code
-# code.interact(local=locals())
+pocketMortyWikiUrls = [baseUrl + tag['href'] for tag in mortyTableEntries]
+mortyWikiSoups = [soupify(url) for url in pocketMortyWikiUrls]
+mortyImageUrlsAndNames = [getPocketMortyImgUrlAndName(soup) for soup in mortyWikiSoups]
+[download(url, name) for url, name in mortyImageUrlsAndNames]
