540aabe6bc7cc93f971d604aca196ff4afc8f4e5
Joshua Cristol <jcrstiol@spredfast.com>
2017-08-02 16:32:42 -0500

    i really like what is on this branch right now just need to look for the stuff you want in each page

--- pokedex/tutorial/tutorial/spiders/quotes_spider.py
+++ pokedex/tutorial/tutorial/spiders/quotes_spider.py
@@ -1,55 +1,18 @@
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
+class MySpider(CrawlSpider):
+    name = 'morty'
+    start_urls = ["http://rickandmorty.wikia.com/wiki/List_of_Mortys_(Pocket_Mortys)"]
+    rules = (
+        Rule(LinkExtractor(allow=('wiki/.*Morty$', )), callback='parse_item'),
+    )
 
-class QuotesSpider(scrapy.Spider):
-    base_url = "http://rickandmorty.wikia.com/"
-    name = "morty"
-    start_urls = [
-        "http://rickandmorty.wikia.com/wiki/List_of_Mortys_(Pocket_Mortys)"
-    ]
-    homeCrawled = False
-
-    def homeCrawler(self, response):
-        if self.homeCrawled:
-            return
-        for page in response.css('h1[class="page-header__title"]::text'):
-            title = page.extract()
-            if type(title) is unicode and title == "List of Mortys (Pocket Mortys)":
-                for table in response.css('table[class="wikitable sortable"]'):
-                    self.links = table.css('a[href$="Morty"]').css('a::attr(href)')
-                    self.homeCrawled = True
-
-
-
-    def parse(self, response):
-        # for table in response.css('table[class="wikitable sortable"]'):
-        #     yield {
-        #         'links' : table.css('a[href$="Morty"]').extract()
-        #     }
-        # next_page
-        # links = []
-        # for table in response.css('table[class="wikitable sortable"]'):
-        #     links = table.css('a[href$="Morty"]').css('a::attr(href)')
-        # next_page = links.extract_first()
-        # print "yo"
-        # print next_page
-        # if next_page is not None:
-        #     next_page = response.urljoin(next_page)
-        #     print "hmm"
-        #     print next_page
-        #     yield scrapy.Request(next_page, callback=self.parse)
-
-
-        #home page crawler collect all relevant links
-        self.homeCrawler(response)
-        #call a morty page crawler on each link
-
-        #next page to crawl logic
-        if self.links:
-            next_page = self.links.extract_first()
-            import code
-            code.interact()
-        else:
-            yield{
-                #all the stuff
-            }
+    def parse_item(self, response):
+        self.logger.info('A page! %s', response.url)
+        title = response.css('h1[class="page-header__title"]::text')
+        article = response.css('article[id="WikiaMainContent"]')
+        yield {
+            'title' : title.extract()
+        }
