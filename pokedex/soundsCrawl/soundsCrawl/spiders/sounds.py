c1fdee2020ebd71609ebb369e9beb75908d0ea70
Joshua Cristol <jcrstiol@spredfast.com>
2017-08-02 18:54:33 -0500

    json for images and sounds

--- pokedex/soundsCrawl/soundsCrawl/spiders/sounds.py
+++ pokedex/soundsCrawl/soundsCrawl/spiders/sounds.py
@@ -1,15 +1,21 @@
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from bs4 import BeautifulSoup
 
 
 
 class SoundsSpider(scrapy.Spider):
     name = 'sounds'
     start_urls = ['https://peal.io/soundboards/rick-and-morty']
-    rules = (
-        Rule(LinkExtractor(allow=('.*sound.peal.io/ps/audios.*\.wav.*', )), callback='parse_item'),
-    )
 
-    def parse_item(self, response):
-        pass
+    def parse(self, response):
+        players = response.css('audio.player')
+        grossLinks = players.css('source').extract()
+        prettyLinks = []
+        for link in grossLinks:
+            soup = BeautifulSoup(link, 'html.parser')
+            prettyLinks.append(soup.source['src'])
+        yield {
+            'links' : prettyLinks
+        }
