0846d8a22cf03219104b459601edb14c2e11e3a8
Joshua Cristol <jcrstiol@spredfast.com>
2017-08-07 12:48:27 -0500

    two sketches

---
+++ pokedex/pokedex/sketch2.js
@@ -0,0 +1,9 @@
+function setup() {
+  var myCanvas = createCanvas(255, 175);
+  myCanvas.parent('stats');
+}
+
+function draw() {
+  // These commands are applied to the graphics canvas as normal.
+  background(255, 255, 255);
+}
