31f30ccec5514537a6ff312a2e76dbc74bc3acb9
Joshua Cristol <jcrstiol@spredfast.com>
2017-08-08 11:21:08 -0500

    me likes this morty crawler have not run it

--- pokedex/pokedexCrawlers/pokedexCrawlers/spiders/mortyCrawler.py
+++ pokedex/pokedexCrawlers/pokedexCrawlers/spiders/mortyCrawler.py
@@ -4,22 +4,8 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from bs4 import BeautifulSoup
 
-<<<<<<< HEAD:pokedex/pokedexCrawlers/pokedexCrawlers/spiders/mortyCrawler.py
 class MortycrawlerSpider(CrawlSpider):
     name = 'mortyCrawler'
-=======
-#helper functions
-def evoDicts(soup):
-    res = []
-    allLinks = soup.find_all('a')
-    for link in allLinks:
-        img = link.img
-        res.append({'link' : img["data-src"]})
-    return res
-
-class MySpider(CrawlSpider):
-    name = 'morty'
->>>>>>> edfaa62dd7c7c1c3e359f94bb288be03cc493655:pokedex/mortyCrawl/mortyCrawl/spiders/morty.py
     start_urls = ["http://rickandmorty.wikia.com/wiki/List_of_Mortys_(Pocket_Mortys)"]
     rules = (
         Rule(LinkExtractor(allow=('wiki/.*Morty$', )), callback='parse_item'),
@@ -58,9 +44,5 @@ class MySpider(CrawlSpider):
                 'type_info' : {'name' : typeSoup.img['alt'].strip().strip("\""), 'file_name' : typeSoup.img["data-image-key"].strip().strip("\""), 'src' : typeSoup.img['src'].strip().strip("\"")},
                 'body_info' : {'height' : bodyInfoSoup.td.next_sibling.text.strip().strip("\""), 'weight' : bodyInfoSoup.td.next_sibling.next_sibling.next_sibling.text.strip().strip("\"")}
             },
-<<<<<<< HEAD:pokedex/pokedexCrawlers/pokedexCrawlers/spiders/mortyCrawler.py
-=======
-            'evolution_info' : evoDicts(evolutionSoup),
->>>>>>> edfaa62dd7c7c1c3e359f94bb288be03cc493655:pokedex/mortyCrawl/mortyCrawl/spiders/morty.py
             'campaign_info' : {'badges_req' : campaignInfoSoup.td.next_sibling.text.strip().strip("\""), 'rare' : campaignInfoSoup.td.next_sibling.next_sibling.next_sibling.text.strip().strip("\"")}
         }
