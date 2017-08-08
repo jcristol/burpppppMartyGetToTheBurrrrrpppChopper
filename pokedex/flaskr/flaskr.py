0543c81d7b16fbe102b6044747ac2b388aaab55e
Joshua Cristol <jcrstiol@spredfast.com>
2017-08-08 13:05:31 -0500

    wooooooo

--- pokedex/flaskr/flaskr.py
+++ pokedex/flaskr/flaskr.py
@@ -6,6 +6,7 @@ from flask import Flask, render_template, redirect, url_for
 import StringIO
 
 app = Flask(__name__)
+
 # The scraped Data yoooooo!!!!!!!!!!!!!!!!!!!!!!!!!!
 mortyDataPaths = []
 mortys = []
@@ -20,35 +21,17 @@ infoJsonFiles = [open(path, 'r') for path in infoPaths]
 infoJson = [json.loads(f.read()) for f in infoJsonFiles]
 
 
-# @app.route('/mortydex/<morty>')
-# def morty_dex(morty):
-    # try:
-    #     index = mortys.index(morty)
-    # except ValueError:
-    #     print "Ooppps thats not a morty"
-    #     return redirect(url_for('index'))
-    # big_image = imagePaths[index][1:]
-    # mortyInfo = infoJson[index]
-    # # return render_template('hello.html', image=big_image, info=mortyInfo)
-    # return render_template('pokedex.html')
-#
-# @app.route('/mortys')
-# def base():
-#     return redirect(url_for('morty_dex', morty='Ace_Pilot_Morty'))
-
-@app.route('/mortys')
-def base():
+@app.route('/mortydex/<morty>')
+def display_morty(morty):
     try:
-        index = mortys.index('Ace_Pilot_Morty')
+        index = mortys.index(morty)
     except ValueError:
         print "Ooppps thats not a morty"
         return redirect(url_for('index'))
     big_image = imagePaths[index][1:]
     mortyInfo = infoJson[index]
     return render_template('pokedex.html', image=big_image, info=mortyInfo)
-    # return render_template('pokedex.html')
 
 @app.route('/')
 def index():
     return "Hello your at the index"
-    # return render_template('pokedex.html')
