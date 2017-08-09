b65de263f555e1adddb69d76d27ae6b56703b655
jcristol <joshcristol@gmail.com>
2017-08-08 20:59:33 -0500

    need a button

--- pokedex/pokedex/pokedex.py
+++ pokedex/pokedex/pokedex.py
@@ -4,6 +4,7 @@ import os
 import json
 from flask import Flask, render_template, redirect, url_for
 import StringIO
+import random
 
 app = Flask(__name__)
 
@@ -20,9 +21,16 @@ infoPaths = [(path + "/info.json") for path in mortyDataPaths]
 infoJsonFiles = [open(path, 'r') for path in infoPaths]
 infoJson = [json.loads(f.read()) for f in infoJsonFiles]
 
+#sounds i "scraped"
+soundPaths = []
+for filename in os.listdir('./static/sounds'):
+    if filename.endswith(".wav"):
+        soundPaths.append("./static/sounds/" + filename)
+
 
 @app.route('/mortydex/<morty>')
 def display_morty(morty):
+    randomSound = random.choice(soundPaths)[1:]
     try:
         index = mortys.index(morty)
     except ValueError:
@@ -30,8 +38,8 @@ def display_morty(morty):
         return redirect(url_for('index'))
     big_image = imagePaths[index][1:]
     mortyInfo = infoJson[index]
-    return render_template('pokedex.html', image=big_image, info=mortyInfo)
+    return render_template('pokedex.html', image=big_image, info=mortyInfo, noise=randomSound)
 
 @app.route('/')
 def index():
-    return "Hello your at the index"
+    return "Hello you're at the index"
